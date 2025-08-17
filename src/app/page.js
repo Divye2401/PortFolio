7b386e05b98695ab3884da69dc91a33735d8eeeb
Divye2401 <52701687+Divye2401@users.noreply.github.com>
2025-08-17 02:38:24 -0400

    done jg

--- src/app/page.js
+++ src/app/page.js
@@ -214,7 +214,7 @@ export default function Home() {
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
-                  transition={{ duration: 0.5, delay: 0 }}
+                  transition={{ duration: 1, delay: 0 }}
                   onViewportLeave={() => {
                     setShowMsg(false);
                     setShowFinalMsg(true);
